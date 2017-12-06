80cf2446b4be52c0a6d84ca346e8fec6ec4dbfef
Kristina Gelžinytė <kristina.gelzinyte@gmail.com>
2017-12-05 18:29:44 -0800

    Added quit button node to game scene.

--- Helicopter-2D/Sprites/HudNode.swift
+++ Helicopter-2D/Sprites/HudNode.swift
@@ -15,19 +15,34 @@ class HudNode: SKNode {
     private var highScore: Int = 0
     ///Defines if highest score is shown now.
     private var showsHighScore = false
+    ///Defines if quit button is pressed.
+    private(set) var quitButtonPressed = false
+    
+    //Init of quit button.
+    private var quitButton: SKSpriteNode!
+    private let quitButtonTexture = SKTexture(imageNamed: "menuQuit")
+    private let quitButtonPressedTexture = SKTexture(imageNamed: "menuQuitP")
+    
     
     ///Sets up HUD node.
     public func setup(size: CGSize){
+        
+        //Sets up score label.
         let defaults = UserDefaults.standard
-        
         highScore = defaults.integer(forKey: ScoreKey)
-        
         scoreNode.text = "\(score)"
         scoreNode.fontSize = 50
-        scoreNode.position = CGPoint(x: size.width / 2, y: size.height - 100)
+        scoreNode.position = CGPoint(x: size.width - 60, y: size.height - 120)
         scoreNode.zPosition = 1
-        
         addChild(scoreNode)
+        
+        //Sets up quit button.
+        quitButton = SKSpriteNode(texture: quitButtonTexture)
+        quitButton.size = CGSize(width: quitButton.size.width * 1.5, height: quitButton.size.height * 1.5)
+        let margin: CGFloat = 35
+        quitButton.position = CGPoint(x: size.width - quitButton.size.width + margin, y: size.height - quitButton.size.height)
+        quitButton.zPosition = 10000
+        addChild(quitButton)
     }
     
     
